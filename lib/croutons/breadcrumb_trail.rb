98b35aff1cc6674594bba76088126155f9cb75b8
Robert Audi <robert.audii@gmail.com>
2016-09-30 14:41:40 +0200

    Adjust the method definitions of `method_missing` and `respond_to_missing?` as per the official documentation
    Let `respond_to_missing?' take a second optional parameter (as it should be.)
    Here is the documentation for the `respond_to_missing?` method on `Object`.
    
    Note: I linked to the documentation for Ruby 2.1.3 because that's the version
    specified in the `.ruby-version` file at the root of the project.
    
    http://ruby-doc.org/core-2.1.3/Object.html#method-i-respond_to_missing-3F
    
    I also amended the definition of the `method_missing` method to include the
    block parameter.

--- lib/croutons/breadcrumb_trail.rb
+++ lib/croutons/breadcrumb_trail.rb
@@ -15,7 +15,7 @@ module Croutons
       build_breadcrumbs
     end
 
-    def method_missing(name, *args)
+    def method_missing(name, *args, &block)
       if respond_to_missing?(name)
         Rails.application.routes.url_helpers.public_send(name, *args)
       else
@@ -23,8 +23,8 @@ module Croutons
       end
     end
 
-    def respond_to_missing?(name)
-      Rails.application.routes.url_helpers.respond_to?(name)
+    def respond_to_missing?(name, include_private = false)
+      Rails.application.routes.url_helpers.respond_to?(name) || super
     end
 
     private
