9fb2a092568e8568025e9ab3ae1c9641baf20584
Maher Hawash <gmhawash@gmail.com>
2022-05-23 07:51:00 -0700

    Add helpers last_breadcrumb, breadcrumb_trail

--- lib/croutons/controller.rb
+++ lib/croutons/controller.rb
@@ -2,6 +2,8 @@ module Croutons
   module Controller
     def self.included(controller)
       controller.helper_method(:breadcrumbs)
+      controller.helper_method(:last_breadcrumb)
+      controller.helper_method(:breadcrumb_trail)
     end
 
     def render_to_body(options)
@@ -13,18 +15,29 @@ module Croutons
     private
 
     def breadcrumbs(objects = {})
-      template = lookup_context.find_template(@_template, @_prefixes)
-      template_identifier = template.virtual_path.gsub('/', '_')
-      objects.reverse_merge!(view_assigns)
-      objects[:params] = params
-      breadcrumbs = breadcrumb_trail.breadcrumbs(template_identifier, objects)
       render_to_string(
         partial: 'breadcrumbs/breadcrumbs',
-        locals: { breadcrumbs: breadcrumbs },
+        locals: { breadcrumbs: breadcrumb_trail(objects) },
       )
     end
 
-    def breadcrumb_trail
+    def last_breadcrumb
+      breadcrumb_trail.last
+    end
+
+    def breadcrumb_trail(objects = {})
+      @breadcrumb_trail ||= begin
+        return [] unless @_template.present?
+
+        template = lookup_context.find_template(@_template, @_prefixes)
+        template_identifier = template.virtual_path.gsub('/', '_')
+        objects.reverse_merge!(view_assigns)
+        objects[:params] = params
+        breadcrumb_trail_class.breadcrumbs(template_identifier, objects)
+      end
+    end
+
+    def breadcrumb_trail_class
       ::BreadcrumbTrail
     rescue NameError
       raise NotImplementedError,
